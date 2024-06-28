263c610e3b30e6b896bb06089ea1c0d85242a12e
Pro Dot Net <141953307+prodotnet@users.noreply.github.com>
2024-06-28 02:30:51 +0200

    No Api

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from "./components/home/home.component";
+
+
+
+
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, NavbarComponent, FooterComponent, HomeComponent]
+})
+export class AppComponent {
+  title = 'OnlineBankingSystem.UI';
+}
+
+
